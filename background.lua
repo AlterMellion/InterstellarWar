2cb5ff1d263006256965853f1d146a085db04e67
AlterMellion <115452735+AlterMellion@users.noreply.github.com>
2025-07-07 22:00:56 +0200

    Add background sound track

--- background.lua
+++ background.lua
@@ -2,9 +2,14 @@ local background = {}
 
 local scrollingSpeed = 25
 local y = 0
+local sound
 
 function background.load()
     background = love.graphics.newImage("pics/spaceBackground.jpg")
+    sound = love.audio.newSource("audio/hyperion-hypercube-355494.mp3", "static")
+    sound:setLooping(true)
+    sound:stop()
+    sound:play()
 end
 
 function background.draw()
